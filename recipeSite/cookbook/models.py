1f5cfc466d22d8acf1b4319f544b6a11cfb49061
Cassandra de la Cruz-Munoz <me@csas-dlcm.dev>
2023-09-16 00:41:15 -0400

    add basic schema

--- recipeSite/cookbook/models.py
+++ recipeSite/cookbook/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+# https://schema.org/NutritionInformation
+# class NutritionInformation(models.Model):
+
+# class RestrictedDiet(models.Model):
+
+
+# https://schema.org/Recipe
+class Recipe(models.Model):
+    # From Recipe
+    cookTime = models.DurationField()
+    cookingMethod = models.TextField()
+    # nutrition = models.OneToOneField(NutritionInformation, on_delete=models.CASCADE)
+    recipeCategory = models.TextField()
+    recipeCuisine = models.TextField()
+    recipeYieldAmount = models.IntegerField()
+    recipeYieldUnits = models.TextField()
+    # restrictedDiet = models.ManyToManyField(RestrictedDiet)
+    # From HowTo
+    estimatedCost = models.DecimalField(max_digits=10, decimal_places=2)
+    preformTime = models.DurationField()
+    prepTime = models.DurationField()
+    totalTime = models.DurationField()
+    author = models.TextField()
+    datePublished = models.DateField()
+    description = models.TextField()
+    name = models.CharField(max_length=100)
+
+    def tools(self):
+        return self.recipetool_set
+
+    def __str__(self):
+        return self.name
+
+class RecipeInstruction(models.Model):
+    stepNumber = models.PositiveIntegerField()
+    instruction = models.TextField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.stepNumber) + self.instruction.__str__()
+
+
+class RecipeIngredient(models.Model):
+    amount = models.IntegerField()
+    unit = models.TextField()
+    item = models.TextField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.amount) + self.unit.__str__() + self.item.__str__()
+
+
+class RecipeSupply(models.Model):
+    amount = models.IntegerField()
+    unit = models.TextField()
+    item = models.TextField()
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.amount) + self.unit.__str__() + self.item.__str__()
+
+
+class RecipeTool(models.Model):
+    item = models.TextField()
+    recipe = models.ManyToManyField(Recipe)
+
+    def __str__(self):
+        return self.item
